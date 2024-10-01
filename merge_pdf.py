e35498aead19db72baa906e7155bef096b413cb4
Carlos Escudero <escuderocarlos23@gmail.com>
2024-09-30 22:38:58 -0500

    Update file merging logic to include additional category and correct output paths for PDF files

--- merge_pdf.py
+++ merge_pdf.py
@@ -2,49 +2,53 @@ from PyPDF2 import PdfMerger
 import os
 import shutil
 
-def mergedFiles(cat:str , url_files:str):
-    
-    names_files=os.listdir(url_files)
+
+def mergedFiles(cat: str, url_files: str, fig_cat: str):
+
+    names_files = os.listdir(url_files)
 
     for name_file in names_files:
 
         file_path = url_files + "/" + name_file
-        lenfile= os.listdir(file_path)
+        lenfile = os.listdir(file_path)
 
-        if len(lenfile) > 9:
+        if len(lenfile) > 18:
 
-            pdfs=[f"C:/Users/Seozz_/Downloads/{name_file} pag1.pdf",f"C:/Users/Seozz_/Downloads/{name_file} pag2.pdf"]
-            name_out=f"C:/Users/Seozz_/Downloads/TRABAJO/POKAL/TIMON/{cat}/{name_file}.pdf"
+            pdfs = [
+                f"C:/Users/Seozz_/Desktop/pdf-fig/{fig_cat}/{name_file}-1.pdf",
+                f"C:/Users/Seozz_/Desktop/pdf-fig/{fig_cat}/{name_file}-2.pdf",
+            ]
+            print(pdfs)
+            name_out = f"C:/Users/Seozz_/Desktop/CopaT/{cat}/{name_file}.pdf"
             fusionar = PdfMerger()
 
             for pdf in pdfs:
                 fusionar.append(open(pdf, "rb"))
             with open(name_out, "wb") as salida_file:
                 fusionar.write(salida_file)
-     
+
         else:
+            print(f"C:/Users/Seozz_/Desktop/pdf-fig/{fig_cat}/{name_file}.pdf")
+            shutil.move(f"C:/Users/Seozz_/Desktop/pdf-fig/{fig_cat}/{name_file}.pdf", f"C:/Users/Seozz_/Desktop/CopaT/{cat}/")
 
-            shutil.move(f"C:/Users/Seozz_/Downloads/{name_file}.pdf", f"C:/Users/Seozz_/Downloads/TRABAJO/POKAL/TIMON/{cat}/")
-            
-      
 
-def get_List(url_files:str):
+def get_List(url_files: str):
     name_files = os.listdir(url_files)
     for name_file in name_files:
         file_path = url_files + "/" + name_file
-        lenfile= os.listdir(file_path)
+        lenfile = os.listdir(file_path)
         if len(lenfile) > 9:
-            print(name_file,"pag1")
-            print(name_file,"pag2")
+            print(name_file, "-1")
+            print(name_file, "-2")
 
         else:
             print(name_file)
 
 
+url_files = r"C:\Users\Seozz_\Desktop\CopaT\Cat 07\IMG"
+category = "Cat 07"
+fig_cat = "cat07"
 
-url_files = r"C:\Users\Seozz_\Downloads\TRABAJO\POKAL\TIMON\2012\IMG"
-category = "2012"
+# get_List(url_files)
 
-get_List(url_files)
-
-mergedFiles(category, url_files)
+mergedFiles(category, url_files, fig_cat)
