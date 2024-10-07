6465b84fa1e4dda061a9dbff29279f6cf92a49f3
Carlos Escudero <escuderocarlos23@gmail.com>
2024-10-07 15:06:36 -0500

    Refactor mergedFiles function in merge_pdf.py to simplify parameters and improve category handling in file paths

--- merge_pdf.py
+++ merge_pdf.py
@@ -4,7 +4,7 @@ import shutil
 import const
 
 
-def mergedFiles(cat: str, url_files: str, fig_cat: str):
+def mergedFiles(categoria: str, url_files: str):
 
     names_files = os.listdir(url_files)
     print(names_files)
@@ -17,11 +17,11 @@ def mergedFiles(cat: str, url_files: str, fig_cat: str):
         if len(lenfile) > 18:
 
             pdfs = [
-                f"{const.URL_PDF_FIG}/{fig_cat}/{name_file}-1.pdf",
-                f"{const.URL_PDF_FIG}/{name_file}-2.pdf",
+                f"{const.URL_PDF_FIG}/{categoria}/{name_file}-1.pdf",
+                f"{const.URL_PDF_FIG}/{categoria}/{name_file}-2.pdf",
             ]
             print(pdfs)
-            name_out = f"{const.URL_BASE}/{cat}/{name_file}.pdf"
+            name_out = f"{const.URL_BASE}/{categoria}/{name_file}.pdf"
             fusionar = PdfMerger()
 
             for pdf in pdfs:
@@ -30,16 +30,15 @@ def mergedFiles(cat: str, url_files: str, fig_cat: str):
                 fusionar.write(salida_file)
 
         else:
-            print(f"{const.URL_PDF_FIG}/{fig_cat}/{name_file}.pdf")
+            print(f"{const.URL_PDF_FIG}/{categoria}/{name_file}.pdf")
             shutil.move(
-                f"{const.URL_PDF_FIG}/{fig_cat}/{name_file}.pdf",
-                f"{const.URL_OUTPUT}/{cat}/",
+                f"{const.URL_PDF_FIG}/{categoria}/{name_file}.pdf",
+                f"{const.URL_BASE}/{categoria}/",
             )
 
 
-categoria = "Cat 11"
-fig_cat = "cat11"
+categoria = "Cat 12"
 url_files = const.URL_BASE + "/" + categoria + "/IMG"
 
 
-mergedFiles(categoria, url_files, fig_cat)
+mergedFiles(categoria, url_files)
